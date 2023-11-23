462cf926d4c11a9ac7631bd7a67e57a4aa9cc4c7
willian mustafa <willian@integra.do>
2023-11-23 14:34:09 -0300

    Tournament init

--- frontend/src/pages/Results.tsx
+++ frontend/src/pages/Results.tsx
@@ -1,16 +1,20 @@
 import TeamSelect from "components/team-select";
+import TournamentBracket from "components/tournament-bracket";
 import React from "react";
 
 export default function Results() {
   return (
     <div className="row">
-      <div className="d-flex gap-4">
+      <div className="d-flex gap-4 col-12">
         <div className="col-4">
           <TeamSelect />
         </div>
         <div className="col-5">
           <button className="btn btn-primary">Simular</button>
         </div>
+      </div>
+      <div className="col-12 mt-5">
+        <TournamentBracket />
       </div>
     </div>
   );
