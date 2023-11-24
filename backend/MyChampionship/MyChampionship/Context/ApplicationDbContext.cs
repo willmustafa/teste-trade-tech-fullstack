f574236f6624be2dd6adb4510c77d25e9a1626c3
willmustafa <willianfmustafa@gmail.com>
2023-11-24 12:26:33 -0300

    Adds Team

--- backend/MyChampionship/MyChampionship/Context/ApplicationDbContext.cs
+++ backend/MyChampionship/MyChampionship/Context/ApplicationDbContext.cs
@@ -11,5 +11,7 @@ namespace MyChampionship.Context
         }
 
         public DbSet<User> Users { get; set; }
+
+        public DbSet<MyChampionship.Models.Team>? Team { get; set; }
     }
 }
---
+++ backend/MyChampionship/MyChampionship/Models/Team.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MyChampionship.Models
+{
+    public class Team
+    {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string Name { get; set; } = null!;
+    }
+}
